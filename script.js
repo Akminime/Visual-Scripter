261a874b3ff1b2cc9e7b3d2c22bc221d3b9f9a2a
Aiden Gonka <akminime@yahoo.com>
2024-12-18 13:48:56 -0900

    Wierd html issue fixes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const canvas = document.getElementById('canvas');
 
 nodes.forEach(node => {
     node.addEventListener('dragstart', (e) => {
-        e.dataTransfer.setData('text/plain', node.id);  // Store the node id in the dataTransfer object
+        e.dataTransfer.setData('text/plain', node.id);
     });
 });
 
@@ -15,7 +15,7 @@ canvas.addEventListener('drop', (e) => {
     e.preventDefault();
     const nodeId = e.dataTransfer.getData('text/plain');
     const nodeElement = document.getElementById(nodeId);
-    
+
     const clonedNode = nodeElement.cloneNode(true);
     const xPos = e.clientX - canvas.offsetLeft;
     const yPos = e.clientY - canvas.offsetTop;
@@ -24,36 +24,5 @@ canvas.addEventListener('drop', (e) => {
     clonedNode.style.left = `${xPos}px`;
     clonedNode.style.top = `${yPos}px`;
 
-    canvas.appendChild(clonedNode);
-
-    makeNodeMovable(clonedNode);
+    canvas.appendChild(clonedNode)
 });
-
-function makeNodeMovable(node) {
-    let isDragging = false;
-    let offsetX, offsetY;
-
-    node.addEventListener('mousedown', (e) => {
-        isDragging = true;
-
-        offsetX = e.clientX - node.offsetLeft;
-        offsetY = e.clientY - node.offsetTop;
-
-        node.style.cursor = 'move';
-    });
-
-    document.addEventListener('mousemove', (e) => {
-        if (isDragging) {
-            const xPos = e.clientX - offsetX;
-            const yPos = e.clientY - offsetY;
-
-            node.style.left = `${xPos}px`;
-            node.style.top = `${yPos}px`;
-        }
-    });
-
-    document.addEventListener('mouseup', () => {
-        isDragging = false;
-        node.style.cursor = 'grab';
-    });
-}
